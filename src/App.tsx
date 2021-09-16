baf7b0ca4ee28cf21844a2bd7da3d8eb880e4cd4
tommy <tommy.dev@pgpg.la>
2021-09-17 03:33:52 +0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import QrReader from 'react-qr-reader'
+import { Container, Row, Col, Card } from "react-bootstrap";
+import _ from "lodash";
 
+const categoryList = [
+  "精美點心",
+  "即拉腸粉",
+  "自製包點",
+  "煎炸點心",
+  "甜美糕點",
+  "田園蔬菜",
+  "皇牌蒸飯",
+  "粥類",
+  "飲品",
+  "其他",
+];
+const itemList = [
+  { name: "（蒸）上海菜肉包", price: 7, catId: 3, shortForm: "蒸 菜肉" },
+  { name: "（煎）上海菜肉包", price: 7, catId: 3, shortForm: "煎 菜肉" },
+  { name: "1蚊", price: 1, catId: 10, shortForm: "1蚊" },
+  { name: "隨心蝦餃皇", price: 27, catId: 1, shortForm: "蝦餃皇" },
+  { name: "韭菜鮮蝦餃", price: 22, catId: 1, shortForm: "韭菜餃" },
+  { name: "蟹柳滑雞扎", price: 20, catId: 1, shortForm: "雞扎" },
+  { name: "鵪鶉蛋燒賣", price: 20, catId: 1, shortForm: "鵪鶉蛋" },
+  { name: "蟹籽燒賣皇", price: 20, catId: 1, shortForm: "燒賣" },
+  { name: "牛肉燒賣皇", price: 18, catId: 1, shortForm: "牛肉燒賣" },
+  { name: "蠔油鮮竹卷", price: 19, catId: 1, shortForm: "鮮竹卷" },
+  { name: "醬皇蒸鳳爪", price: 19, catId: 1, shortForm: "鳳爪" },
+  { name: "足料糯米雞", price: 25, catId: 1, shortForm: "糯米雞" },
+  { name: "排骨白腸粉", price: 35, catId: 1, shortForm: "排骨腸粉" },
+  { name: "豉汁蒸排骨", price: 19, catId: 1, shortForm: "排骨" },
+  { name: "鮮竹牛肉球", price: 19, catId: 1, shortForm: "牛肉球" },
+  { name: "招牌珍珠雞 (一隻)", price: 10, catId: 1, shortForm: "珍珠 一隻" },
+  { name: "招牌珍珠雞 (兩隻)", price: 18, catId: 1, shortForm: "珍珠 兩隻" },
+  { name: "招牌珍珠雞 (三隻)", price: 25, catId: 1, shortForm: "珍珠 三隻" },
+  { name: "隨心鮮蝦腸", price: 28, catId: 2, shortForm: "蝦腸" },
+  { name: "蜜汁叉燒腸", price: 24, catId: 2, shortForm: "叉燒腸" },
+  { name: "香茜牛肉腸", price: 24, catId: 2, shortForm: "牛肉腸" },
+  { name: "竹笙羅漢齋腸", price: 24, catId: 2, shortForm: "羅漢齋腸" },
+  { name: "脆皮春卷腸", price: 24, catId: 2, shortForm: "春卷腸" },
+  { name: "葱花蝦米腸", price: 24, catId: 2, shortForm: "蝦米腸" },
+  { name: "混醬布拉腸", price: 20, catId: 2, shortForm: "布拉腸" },
+  { name: "蜜汁叉燒包", price: 7, catId: 3, shortForm: "叉燒" },
+  { name: "鬆軟馬拉糕", price: 7, catId: 3, shortForm: "馬拉" },
+  { name: "鳳凰奶皇包", price: 7, catId: 3, shortForm: "奶皇" },
+  { name: "黑芝麻蓉包", price: 7, catId: 3, shortForm: "芝麻" },
+  { name: "家鄉糯米卷", price: 7, catId: 3, shortForm: "糯米卷" },
+  { name: "北方白饅頭", price: 6, catId: 3, shortForm: "饅頭" },
+  { name: "鮮蝦春卷", price: 18, catId: 4, shortForm: "春卷" },
+  { name: "鮮蝦腐皮卷", price: 18, catId: 4, shortForm: "腐皮卷" },
+  { name: "香煎蘿蔔糕", price: 18, catId: 4, shortForm: "蘿蔔糕" },
+  { name: "脆炸雲吞", price: 16, catId: 4, shortForm: "雲吞" },
+  { name: "蓮蓉煎堆仔", price: 16, catId: 4, shortForm: "煎堆" },
+  { name: "咸水角", price: 18, catId: 4, shortForm: "咸水" },
+  { name: "養顏姜汁糕(三件)", price: 12, catId: 5, shortForm: "姜汁糕" },
+  { name: "滋潤紅枣糕(三件)", price: 12, catId: 5, shortForm: "紅枣糕" },
+  { name: "白灼菜芯", price: 18, catId: 6, shortForm: "菜芯" },
+  { name: "田園唐生菜", price: 18, catId: 6, shortForm: "生菜" },
+  { name: "鳳爪排骨飯", price: 29, catId: 7, shortForm: "鳳爪排骨" },
+  { name: "北菇滑雞飯", price: 29, catId: 7, shortForm: "北菇雞" },
+  { name: "臘腸滑雞飯", price: 29, catId: 7, shortForm: "臘腸雞" },
+  { name: "(蒸)蛋牛肉飯", price: 29, catId: 7, shortForm: "蒸 蛋牛" },
+  { name: "(煎)蛋牛肉飯", price: 29, catId: 7, shortForm: "煎 蛋牛" },
+  { name: "竹笙羅漢齋飯", price: 29, catId: 7, shortForm: "羅漢齋" },
+  { name: "南瓜排骨飯", price: 29, catId: 7, shortForm: "南瓜" },
+  { name: "荷葉蟲草花蒸滑雞飯", price: 42, catId: 7, shortForm: "蟲草雞" },
+  { name: "荷葉蟲草花蒸排骨飯", price: 42, catId: 7, shortForm: "蟲草排骨" },
+  { name: "皮蛋瘦肉粥", price: 16, catId: 8, shortForm: "皮蛋瘦" },
+  { name: "粟米南瓜粥", price: 16, catId: 8, shortForm: "南瓜" },
+  { name: "軟綿綿白粥", price: 16, catId: 8, shortForm: "白粥" },
+  { name: "汽水", price: 8, catId: 9, shortForm: "汽水" },
+  { name: "土魷肉餅飯", price: 29, catId: 7, shortForm: "肉餅" },
+  { name: "北菇綿花雞", price: 22, catId: 1, shortForm: "北菇雞" },
+  { name: "肉骨茶", price: 17, catId: 11, shortForm: "肉骨茶" },
+];
+
+const ReceiptPage = ({ result }: { result: any[] }) => {
+  const detailedSelectedList = result.map((e) =>
+    itemList.find((item) => item.name == e)
+  );
+  console.log(detailedSelectedList);
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Text style={{ fontSize: 30, textAlign: "center" }}>
+                單號
+              </Card.Text>
+              <Card.Title style={{ textAlign: "center", fontSize: 50 }}>
+                001
+              </Card.Title>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Title style={{ fontSize: 30, textAlign: "center" }}>
+                訂單詳細
+              </Card.Title>
+              {detailedSelectedList.map(
+                (e) =>
+                  e && (
+                    <Card.Text style={{ margin: "10px 0", fontSize: 20 }}>
+                      <Container>
+                        <Row>
+                          <Col xs={5}>{e.name || ""}</Col>
+                          <Col>x 1</Col>
+                          <Col>${e?.price || ""}</Col>
+                        </Row>
+                      </Container>
+                    </Card.Text>
+                  )
+              )}
+              <Card.Text style={{ margin: "10px 0", fontSize: 20 }}>
+                <Container>
+                  <Row>
+                    <Col style={{ fontWeight: "bold" }} xs={5}>
+                      總計
+                    </Col>
+                    <Col />
+                    <Col style={{ fontWeight: "bold" }}>
+                      $
+                      {_.sum(
+                        result.map(
+                          (e) => itemList.find((item) => item.name == e)?.price
+                        )
+                      ) || 0}
+                    </Col>
+                  </Row>
+                </Container>
+              </Card.Text>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QrReader
+      delay={1300}
+      onError={err => alert(err)}
+      onScan={data => alert(data)}
+      style={{ width: "100%" }}
+    />
   );
 }
 
